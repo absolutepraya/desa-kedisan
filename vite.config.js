c255ed85ce940992705ed5746699109022c36073
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-10-14 21:59:17 +0700

    Fixing GH Page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/desa-kedisan/'
+	base: 'https://absolutepraya.github.io/desa-kedisan/'
 });
